77fd8462158888dfbb7eb5ea5d634217c8d58c12
Patricia Paola <maximianopatricia55@gmail.com>
2026-05-30 21:38:03 -0300

    corrigindo links externos

--- script.js
+++ script.js
@@ -1,43 +1,20 @@
-// scroll suave
-document.querySelectorAll("a").forEach(a => {
-    a.addEventListener("click", (e) => {
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function(e) {
+
         e.preventDefault();
 
-        const target = document.querySelector(a.getAttribute("href"));
+        const target =
+            document.querySelector(this.getAttribute("href"));
 
         if(target){
+
             target.scrollIntoView({
-                behavior:"smooth"
+                behavior: "smooth"
             });
+
         }
-    });
-});
 
-// efeito leve nos cards
-const cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-    card.addEventListener("mousemove", (e) => {
-        const rect = card.getBoundingClientRect();
-
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
-
-        const rotateX = (y - centerY) / 15;
-        const rotateY = (x - centerX) / 15;
-
-        card.style.transform = `
-            perspective(1000px)
-            rotateX(${-rotateX}deg)
-            rotateY(${rotateY}deg)
-            translateY(-10px)
-        `;
     });
 
-    card.addEventListener("mouseleave", () => {
-        card.style.transform = "";
-    });
 });
